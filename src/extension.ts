5fa1c9a26c1d21e06a597e0c3ae65676fcf4a72e
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-12-05 14:57:25 +0300

    Auto-update headers via pre_commit.ps1 - 2025-12-05

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 04 декабря 2025 13:20:44
- * Version: 1.0.105
+ * Last Updated: 05 декабря 2025 14:57:25
+ * Version: 1.0.106
  */
 
 import * as vscode from 'vscode';
